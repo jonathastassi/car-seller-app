8d3a219b0fd919acb12086e58eea7f4ce2535b15
Jonathas <jonathastassi@hotmail.com>
2020-09-28 21:08:10 -0300

    Testes unitários do service TokenService

--- web/src/app/auth/services/token.service.spec.ts
+++ web/src/app/auth/services/token.service.spec.ts
@@ -2,15 +2,51 @@ import { TestBed } from '@angular/core/testing';
 
 import { TokenService } from './token.service';
 
-describe('TokenService', () => {
+describe('O serviço TokenService', () => {
   let service: TokenService;
+  let KEY_AUTH = "car_seller_app";
+
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(TokenService);
   });
 
-  it('should be created', () => {
+  it('deve ser criado', () => {
     expect(service).toBeTruthy();
   });
+
+  it('Deve setar um token no localstorage', () => {
+    service.setValue(KEY_AUTH);
+    const token = service.getValue();
+    expect(token).toBe(KEY_AUTH);
+  })
+
+  it('Deve retornar o token do localstorage', () => {
+    service.setValue(KEY_AUTH);
+    const token = service.getValue();
+    expect(token).toBe(KEY_AUTH);
+  })
+
+  it('Deve remover um valor do LocalStorage', () => {
+    service.setValue(KEY_AUTH);
+    service.removeValue();
+    const token = service.getValue();
+    expect(token).toBe(null);
+  });
+
+  it('Deve verificar se existe token no localstorage, retornar true', () => {
+    service.setValue(KEY_AUTH);
+    const hasValue = service.hasValue();
+    expect(hasValue).toBeTrue();
+  });
+
+  it('Deve verificar se não existe token no localstorage, retornar false', () => {
+    const hasValue = service.hasValue();
+    expect(hasValue).toBeFalse();
+  });
+
+  afterEach(() => {
+    localStorage.clear();
+  })
 });
--- web/src/app/shared/components/navigation/navigation.component.spec.ts
+++ web/src/app/shared/components/navigation/navigation.component.spec.ts
@@ -9,6 +9,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { NavigationComponent } from './navigation.component';
 import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('NavigationComponent', () => {
   let component: NavigationComponent;
@@ -25,6 +26,7 @@ describe('NavigationComponent', () => {
         MatListModule,
         MatSidenavModule,
         MatToolbarModule,
+        HttpClientTestingModule,
         RouterTestingModule.withRoutes([])
       ]
     }).compileComponents();
